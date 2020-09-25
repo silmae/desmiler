75d49b0c308039381bc4cbc3537a856fd6ba1f93
kiauriih <kimmo.a.riihiaho@jyu.fi>
2020-09-25 15:39:01 +0300

    Closing session added

--- desmiler/imaging/scanning_session.py
+++ desmiler/imaging/scanning_session.py
@@ -9,16 +9,17 @@ import os
 
 from core import properties as P
 from utilities import file_handling as F
-
+from core.camera_interface import CameraInterface
 
 class ScanningSession:
 
     # Session log? At least some metadata (datetime, camera settings etc.) should be saved I think.
 
-    def __init__(self, session_name:str):
+    def __init__(self, session_name:str, cami:CameraInterface):
         print(f"Creating session '{session_name}'")
         self.session_root = P.scan_folder_name + '/' + session_name
         self.session_name = session_name
+        self._cami = cami
 
         if self.session_exists():
             print(f"Found existing session. Loading files..")
@@ -31,6 +32,12 @@ class ScanningSession:
             return True
         else:
             return False
+
+    def close(self):
+        """Turn camera off and save all stuff."""
+
+        self._cami.turn_off()
+        # TODO save stuff
 
 
 
