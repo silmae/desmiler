306e491409c9828314aec98a8e9d9ecc8afcf04e
Kimmo Riihiaho <kimmo.riihiaho@gmail.com>
2020-09-28 17:57:19 +0300

    Dataset saving with metadata

--- desmiler/utilities/file_handling.py
+++ desmiler/utilities/file_handling.py
@@ -2,6 +2,8 @@
 import os
 import logging
 from core import properties as P
+import xarray as xr
+from xarray import DataArray
 
 def create_default_directories():
     """Creates default structure if it does not exist yet."""
@@ -22,3 +24,32 @@ def create_directory(path:str):
         logging.warning(f"Creation of the directory {path} failed")
     else:
         logging.info(f"Successfully created the directory {path}")
+
+def save_frame(frame:DataArray, path:str, meta_dict):
+    """Saves a frame to the disk with given name.
+
+    File extension '.nc' is added if missing.
+
+    Parameters
+    ----------
+    path : string or path
+        A path to a folder to which the frame should be saved.
+    frame : DataArray
+        The frame to be saved on disk.
+    meta_dict : Dictionary
+        Dictionary of miscellaneous metadata that gets added to DataSet's attributes.
+    """
+
+    frameData = xr.Dataset()
+    frameData[P.naming_frame_data] = frame
+
+    for key in meta_dict:
+        frameData.attrs[key] = meta_dict[key]
+
+    path_s = str(path)
+    if not path_s.endswith('.nc'):
+        path_s = path_s + '.nc'
+
+    # FIXME This fails if the file is already open.
+    frameData.to_netcdf(os.path.normpath(path_s), format='NETCDF4')
+    
