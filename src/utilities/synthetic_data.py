2bb7bb2f4c5a671d5b89a6296e43237c1d152170
Kimmo Riihiaho <kimmo.riihiaho@gmail.com>
2020-10-01 14:05:44 +0300

    Generating undistorted frame

--- src/utilities/synthetic_data.py
+++ src/utilities/synthetic_data.py
@@ -5,6 +5,12 @@ import os
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+import xarray as xr
+import datetime as dt
+import analysis.frame_inspector as frame_inspector
+
+base_path = '../../examples/'
+example_spectrogram_path = base_path + 'fluorescence_spectrogram.nc'
 
 def light_frame_to_spectrogram():
     """Creates a mean spectrogram from few rows of a frame and saves it.
@@ -25,9 +31,44 @@ def light_frame_to_spectrogram():
     frame = frame.mean(dim='y')
     # plt.plot(frame.data)
     # plt.show()
-    save_path = '../../examples/fluorescence_spectrogram'
-    F.save_frame(frame, save_path)
+    F.save_frame(frame, example_spectrogram_path)
+
+def make_undistorted_frame():
+    """Creates an example of undistorted frame to examples directory.
+
+    Frame data follows closely to the form that camazing uses in the frames it provides.
+    Attributes are omitted though.
+    """
+
+    perfect_frame_path = os.path.abspath(base_path + 'undistorted_frame.nc')
+    if not os.path.exists(perfect_frame_path):
+        source = F.load_frame(example_spectrogram_path)
+        destination_frame_height = 800
+        source_data = source.frame.data
+        print(source_data.shape)
+        expanded_data = np.repeat(source_data, destination_frame_height)
+        expanded_data = np.reshape(expanded_data, (source.frame.x.size, destination_frame_height))
+        expanded_data = expanded_data.transpose()
+        print(expanded_data.shape)
+        coords = {
+            "x": ("x", np.arange(0, source.frame.x.size) + 0.5),
+            "y": ("y", np.arange(0, destination_frame_height) + 0.5),
+            "timestamp": dt.datetime.today().timestamp(),
+        }
+        dims = ('y', 'x')
+        frame = xr.DataArray(
+            expanded_data,
+            name="frame",
+            dims=dims,
+            coords=coords,
+        )
+
+        #frame_inspector.plot_frame(frame)
+        F.save_frame(frame, perfect_frame_path)
+    else:
+        print(f"Undistorted example frame already exists in '{perfect_frame_path}'. Doing nothing.")
 
 
 if __name__ == '__main__':
-    light_frame_to_spectrogram()
+    #light_frame_to_spectrogram()
+    make_undistorted_frame()
