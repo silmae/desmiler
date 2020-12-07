cb95c1637719721fa89d259602bd5a08af37a9b7
Kimmo Riihiaho <kimmo.riihiaho@gmail.com>
2020-12-07 09:41:26 +0200

    Improved documentation and light cleanup

--- src/ui.py
+++ src/ui.py
@@ -1,6 +1,12 @@
 """
 
-Console UI for controlling various funktionalities.
+Console user interface intended to be used with ipython. The main at the
+end of the file should contain at least the line "ui = UI()", so that
+running magic command "%run ui.py" in ipython console gives a ready-to-use
+ui object.
+
+Most of the commands expect that there exists a scanning session to connect the data to.
+Scanning sessions are bound to folder "/scans/<scan_name>".
 
 """
 from core.camera_interface  import CameraInterface
@@ -24,14 +30,19 @@ class UI:
     def __init__(self):
         print("Initializing the UI.")
         F.create_default_directories()
-        print("I cannot yet do anything but next, you might like to start "
-              "a named scanning session 'start_session()' for later analysis, "
-              "a freeform session to make quick experiments 'start_freeform_session()' or "
-              "a preview to see camera feed 'start_preview()'.")
+        print("Welcome to desmiler UI! \n")
+        print("You can start by generating examples by running ui.generate_examples().")
+        print("They can be viewed by running ui.show_examples().")
+        print("If you have a connected camera, you may want to run ui.start_preview() to show "
+              "raw video feed from the camera.")
+        print("Run ui.start_session(<session_name>) to start a new session or to open an existing one.")
+        print("If the session contains an existing hyperspectral image cube, you can inspect it by calling "
+              "ui.show_cube().")
 
         scanning_session.create_example_scan()
 
     def __del__(self):
+        # FIXME they are both trying to delete the camera, which will throw an error
         if self.sc is not None:
             self.sc.close()
             del self.sc
@@ -39,7 +50,7 @@ class UI:
             self.preview.close()
             del self.preview
 
-    def start_session(self, session_name:str) -> ScanningSession:
+    def start_session(self, session_name:str):
         """Start new named scanning session with its own folder."""
 
         if self.sc is not None:
@@ -49,36 +60,36 @@ class UI:
         self.sc = ScanningSession(session_name)
         print(f"Session '{self.sc.session_name}' ready.")
 
-    # TODO start_session_copy() copy an existing session as a new session
-
     def start_freeform_session(self):
         """Freeform session will always overwrite old content with the same name."""
 
         self.start_session(P.freeform_session_name)
 
     def close_session(self):
+        """Closes existing session. """
+
         print(f"Closing session '{self.sc.session_name}'")
-        # TODO Check that everything is OK before closing
         self.sc.close()
         del self.sc
 
         print(f"Session closed.")
 
     def close_preview(self):
+        """Closes existing preview."""
+
         if self.preview is not None:
             del self.preview
 
     def start_preview(self):
-        """Start a new preview for inspecting the camera feed."""
+        """Starts a new preview for inspecting the camera feed."""
 
         self.preview = Preview()
         self.preview.start()
 
     def shoot_dark(self):
-        """Shoots and saves a dark frame.
+        """Shoots and saves a dark frame for current session.
 
-        If a session exists, dark should be saved to its folder, otherwise
-        to preview folder.
+        Pauses a preview if one is running.
         """
 
         if self.preview is not None:
@@ -94,10 +105,9 @@ class UI:
             self.preview.start()
 
     def shoot_white(self):
-        """Shoots and saves a white frame.
+        """Shoots and saves a white frame for current session.
 
-        If a session exists, dark should be saved to its folder, otherwise
-        to preview folder.
+        Pauses a preview if one is running.
         """
         if self.preview is not None:
             preview_was_running = self.preview.is_running
@@ -112,10 +122,9 @@ class UI:
             self.preview.start()
 
     def shoot_light(self):
-        """Shoots and saves a peaky light frame.
+        """Shoots and saves a peaky light frame for current session.
 
-        If a session exists, dark should be saved to its folder, otherwise
-        to preview folder.
+        Pauses a preview if one is running.
         """
 
         if self.preview is not None:
@@ -130,14 +139,9 @@ class UI:
         if self.preview is not None and preview_was_running:
             self.preview.start()
 
-    def crop(self, width=None, width_offset=None, height=None, height_offset=None, full=False):
-        # TODO this might be unnecessary as the idea is to control everything with control files. Remove?
-        if self.sc is not None:
-            self.sc.crop(width, width_offset, height, height_offset, full)
-        else:
-            print(f"Asked for cropping but there is no active session to crop.")
-
     def run_scan(self):
+        """Run a scan using active session's control file for scanning parameters."""
+
         if self.preview is not None:
             preview_was_running = self.preview.is_running
             self.preview.stop()
@@ -150,58 +154,67 @@ class UI:
         if self.preview is not None and preview_was_running:
             self.preview.start()
 
-    # def inspect_light(self):
-    #     if self.sc is not None:
-    #         path = self.sc.session_root + '/' + P.ref_light_name
-    #         frame_ds = F.load_frame(path)
-    #         frame_inspector.plot_frame(frame_ds)
-    #         frame_inspector.plot_frame_spectra(frame_ds)
-
-            # f_vals = frame.values
-            # log_frame = frame
-            # log_frame.values = np.log(f_vals)
-            # frame_inspector.plot_frame(frame)
-            # frame_inspector.plot_frame_spectra(log_frame)
-
     def generate_examples(self):
+        """Generates all available examples. This takes quite a long time.
+
+        You can use "synthetic_data.py"'s own methods to generate just some of the examples.
+        """
+
         synthetic.generate_all_examples()
 
     def show_examples(self):
+        """Shows all available examples. """
+
         synthetic.show_frame_examples()
         synthetic.show_cube_examples()
 
     def make_reflectance_cube(self):
+        """Make a reflectance cube using existing raw cube, dark reference, and white reference."""
+
         if self.sc is not None:
             self.sc.make_reflectance_cube()
         else:
-            logging.warning(f"Kääk")
+            logging.warning(f"No active scanning session exists. Cannot create reflectance cube.")
 
     def make_desmiled_cube(self):
+        """Run smile correction for a reflectance cube with interpolated and lookup table shifts."""
+
         if self.sc is not None:
             self.sc.desmile_cube(shift_method=0)
             self.sc.desmile_cube(shift_method=1)
         else:
-            logging.warning(f"Kääk")
+            logging.warning(f"No active scanning session exists. Cannot desmile a cube.")
 
     def show_cube(self):
+        """Start the CubeInspector."""
+
         if self.sc is not None:
             self.sc.show_cube()
         else:
-            logging.warning(f"Kääk")
+            logging.warning(f"No active scanning session exists. Cannot show a cube.")
 
     def show_shift(self):
+        """Show the shift matrix. Use this to check that the matrix looks reasonable."""
+
         if self.sc is not None:
             self.sc.show_shift()
         else:
-            logging.warning(f"Kääk")
+            logging.warning(f"No active scanning session exists. Cannot show the shift matrix.")
 
     def show_light(self):
+        """Show light reference frame to see how well the arc fits fit."""
+
         if self.sc is not None:
             self.sc.show_light()
         else:
-            logging.warning(f"Kääk")
+            logging.warning(f"No active scanning session exists. Cannot show light reference.")
 
     def exposure(self, value=None) -> int:
+        """Set or show the current exposure in microseconds. Useful for preview.
+
+        The run_scan() method does not respect this exposure, but the one set in the control file.
+        """
+
         if self.sc._cami is not None:
             return self.sc.exposure(value)
         elif self.preview is not None:
@@ -210,51 +223,7 @@ class UI:
             logging.warning(f"Cannot set exposure as no Session or Preview exists.")
 
 if __name__ == '__main__':
+    # set logging parameters here
     logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+    # create an UI object to be used from the ipython console
     ui = UI()
-    # ui.start_session(P.example_scan_name)
-    # ui.inspect_light()
-    # sc = ui.sc
-
-    # raw = F.load_cube(os.path.abspath(P.path_rel_scan + P.example_scan_name + '/' + P.cube_reflectance_name + '.nc'))
-    # desmiled = F.load_cube(os.path.abspath(P.path_rel_scan + P.example_scan_name + '/' + P.cube_desmiled_lut + '.nc'))
-    # ci = CubeInspector(raw, desmiled, desmiled, 'reflectance')
-    # ci.show()
-
-    # ui.run_scan()
-    # ui.crop(200,100,150,300)
-    # ui.shoot_dark()x
-    # ui.shoot_white()
-    # ui.shoot_light()
-
-    # ui.start_freeform_session()
-    # ui.show_cube()
-    # ui.run_scan()
-    # ui.inspect_light()
-    ui.start_session('samir')
-    # ui.shoot_light()
-    # ui.shoot_dark()
-    # ui.shoot_white()
-    # ui.make_reflectance_cube()
-    # ui.show_shift()
-    # ui.show_light()
-    # ui.make_desmiled_cube()
-    # ui.run_scan()
-
-    ui.show_cube()
-    # ui.show_shift()
-
-    # cami = CameraInterface()
-    # d = cami._cam.features()
-
-    # for i in range(len(d)):
-    #     print(d[i])
-    # print(f"AcquisitionFrameRateAuto : {cami._cam['AcquisitionFrameRateAuto'].value}")
-    # print(f"SingleFrameAcquisitionMode : {cami._cam['SingleFrameAcquisitionMode'].info()}")
-    # print(f"SingleFrameAcquisitionMode : {cami._cam['SingleFrameAcquisitionMode'].info()}")
-
-    # ui.start_preview()
-    # ui.close_preview()
-    # ui.shoot_dark()
-
-
